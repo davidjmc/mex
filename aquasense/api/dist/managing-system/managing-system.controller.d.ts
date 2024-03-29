045c61e5d891f0f540c4d36f98f9bf21a504fd7e
Angelo Fernandes <67211421+Fernnandez@users.noreply.github.com>
2024-03-29 15:06:43 -0300

    chore(Aquasense): update aquasense to latest version

--- aquasense/api/dist/managing-system/managing-system.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { ManangingSystemService } from './managing-system.service';
-export declare class ManagingSystemController {
-    private readonly manangingSystemService;
-    constructor(manangingSystemService: ManangingSystemService);
-    verifyAdaptation(mac: string): Promise<any>;
-}
--- aquasense/api/dist/mqtt/mqtt.service.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class MqttService {
-    private client;
-    constructor();
-    subscribe(topic: string, callback: (message: string) => void): void;
-    unsubscribe(topic: string): void;
-}
