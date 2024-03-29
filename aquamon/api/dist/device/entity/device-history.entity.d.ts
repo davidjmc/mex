da369a572d736041ebbc6b617784c4be512a46d8
Angelo Fernandes <67211421+Fernnandez@users.noreply.github.com>
2024-03-29 14:56:54 -0300

    chore(Aquamon): remove old version of Aquamon

--- aquamon/api/dist/device/entity/device-history.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Device } from './device.entity';
-export declare class DeviceHistory {
-    id: string;
-    battery: number;
-    water: number;
-    timestamp: Date;
-    device: Device;
-}
